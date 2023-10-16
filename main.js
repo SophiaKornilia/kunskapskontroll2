d231f3b1b0a086bb04d7998a5930916b06328d37
Kornelia Adabugday <seadabugday@gmail.com>
2023-10-16 12:00:03 +0200

    Fixed validationFunction

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ let saveContactBtn = document.getElementById('createBtn');
 let contactList = document.getElementById('contactList');
 let deleteButton = document.getElementById('deleteBtn');
 
+
 saveContactBtn.addEventListener('click', function (e) {
     let inputName = document.getElementById('name').value;
     let inputNumber = document.getElementById('number').value;
 
-    if (inputName.length === 0 || inputNumber.length === 0) {
-        let errorMessage = document.createElement('h3');
-        errorMessage.textContent = "You have to fill both fields with text!";
-        contactList.appendChild(errorMessage);
+    if (validateFunction(inputName, inputNumber, contactList)) {
+        return;
     } else {
         var listItem = document.createElement('li');
 
@@ -43,24 +42,34 @@ saveContactBtn.addEventListener('click', function (e) {
             listItem.remove();
         });
 
-        deleteBtnFunction(); 
+        deleteBtnFunction();
 
     }
-    /**FUNKTIONS */
+    /**FUNCTIONS */
 
+    function validateFunction(inputName, inputNumber, contactList) {
+
+        if (inputName.length === 0 || inputNumber.length === 0) {
+            let errorMessage = document.createElement('h3');
+            errorMessage.textContent = "You have to fill both fields with text!";
+            contactList.appendChild(errorMessage);
+            return true;
+        }
+        return false;
+    }
     function deleteBtnFunction() {
-        deleteButton.addEventListener('click', function(e){
+        deleteButton.addEventListener('click', function (e) {
             listItem.parentNode.remove();
         });
     }
 
-    function changeBtnFunction(){
+    function changeBtnFunction() {
         btn1.addEventListener('click', function () {
             if (nameInput.disabled === true) {
                 nameInput.disabled = false;
                 numberInput.disabled = false;
                 btn1.textContent = 'Save';
-            } else if (nameInput.value.length === 0 || numberInput.value.length === 0){
+            } else if (nameInput.value.length === 0 || numberInput.value.length === 0) {
                 let errorMessage2 = document.createElement('h3');
                 errorMessage2.textContent = "Don´t leave an empty box!";
                 contactList.appendChild(errorMessage2);
